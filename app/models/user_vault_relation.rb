8920a4fa04a38a880b3fd34718f9f069493fddec
Noffica <nmithani1@gmail.com>
2015-07-18 16:41:13 -0700

    Join Vault form validation is done. Custom errors are implemented for the following: • blank vault name • non-existent vault name • an incorrect Invitation Code • already part of vault
    Refactoring of the `post '/user/user_vault'` route is needed for the future.

--- app/models/user_vault_relation.rb
+++ app/models/user_vault_relation.rb
@@ -1,4 +1,12 @@
 class UserVaultRelation < ActiveRecord::Base
   belongs_to :user
   belongs_to :vault
+
+  # before_create :vault_exists?
+
+  private
+  # def vault_exists?
+  #   errors.add(:no_vault,"No vault.") if !Vault.find_by(name: params[:name])
+  # end
+
 end
