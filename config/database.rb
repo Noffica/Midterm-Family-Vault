9b21c646cab516b7550f1e8980ebac97ba2e1d4a
Steffi Au <steffi.myau@gmail.com>
2015-08-04 19:04:35 +0000

    trying without production database setting

--- config/database.rb
+++ config/database.rb
@@ -25,7 +25,7 @@ if development?
     database: "db/db.sqlite3"
   }
 else
-  set :database, ENV['DATABASE_URL']
+  # set :database, ENV['DATABASE_URL']
 end
 
   # Load all models from app/models, using autoload instead of require
